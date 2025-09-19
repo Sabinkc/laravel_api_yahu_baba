6d0aea8681f12cafa2c905ef714a4f1ed16eb642
Sabinkc <sabinkc1206@gmail.com>
2025-09-19 19:14:43 +0545

    login succesful

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -40,41 +40,45 @@ class AuthController extends Controller
         ], 200);
     }
 
-    // public function login(Request $request)
+    public function login(Request $request)
+    {
+
+        $validateUser = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validateUser->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $validateUser->errors()->all(),
+            ]);
+        }
+
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+
+            $authUser = Auth::user();
+            return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfully',
+                'token' => $authUser->createToken('Api token')->plainTextToken,
+                'token_type' => 'bearer',
+
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Credentials do notso match',
+
+            ]);
+        }
+    }
+
+    // public function login()
     // {
-
-    //     $validateUser = Validator::make($request->all(), [
-    //         'name' => 'required',
-    //         'email' => 'required|email|unique:users,email',
-    //         'password' => 'required'
-    //     ]);
-
-    //     if ($validateUser->fails()) {
-    //         return response()->json([
-    //             'status' => false,
-    //             'message' => 'Validation Error',
-    //             'errors' => $validateUser->errors()->all(),
-    //         ]);
-    //     }
-
-
-    //     if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-
-    //         $authUser = Auth::user();
-    //         return response()->json([
-    //             'status' => true,
-    //             'message' => 'User logged in successfully',
-    //             'token' => $authUser->createToken('Api token')->plainTextToken,
-    //             'token_type' => 'bearer',
-
-    //         ], 200);
-    //     } else {
-    //         return response()->json([
-    //             'status' => false,
-    //             'message' => 'Credentials do notso match',
-
-    //         ]);
-    //     }
+    //     return response()->json(['name' => "hello"]);
     // }
 
     public function logut(Request $request)
